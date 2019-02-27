d3443b1f091d0b9cb570725375cda280c9393e49
Fatih <fatig2301@gmail.com>
2019-02-27 23:06:22 +0100

    build.gradle is updated second time

--- src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,5 +1,5 @@
 package test.endtoend.auctionsniper;
-
+/*
 import auctionsniper.Main;
 import auctionsniper.ui.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
@@ -8,11 +8,28 @@ import com.objogate.wl.swing.driver.JLabelDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+*/
+
+
+import auctionsniper.Main;
+import auctionsniper.ui.MainWindow;
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.*;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+
+import static com.objogate.wl.swing.driver.JFrameDriver.*;
+import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
+import static java.lang.String.valueOf;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AuctionSniperDriver extends JFrameDriver {
     public AuctionSniperDriver(int timeoutMillis) {
         super(new GesturePerformer(),
-                JFrameDriver.topLevelFrame(
+                topLevelFrame(
                         named(Main.MAIN_WINDOW_NAME),
                         showingOnScreen()),
                 new AWTEventQueueProber(timeoutMillis, 100));
